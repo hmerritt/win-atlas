94a450c19bff94e64c2d901cf681716748511dab
hmerritt <harry@mrrtt.me>
2024-04-05 18:51:05 +0100

    feat: build script to zip

--- build.ps1
+++ build.ps1
@@ -1,2 +1,7 @@
 
+# Build the playbook
 7z a -tzip -p"malte" -r "Win-Atlas Playbook.apbx" "./src/playbook"
+
+# Copy to, and zip release
+Copy-Item -Path "Win-Atlas Playbook.apbx" -Destination "./src/release-zip" -Force
+Compress-Archive -Path "./src/release-zip/*" -DestinationPath "./atlas-release.zip"
