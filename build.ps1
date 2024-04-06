101bbeaf1b3f4aeca12bf79afe58c41afcad2330
hmerritt <harry@mrrtt.me>
2024-04-06 23:27:47 +0100

    feat: release zip readme

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 
 # Build the playbook
-7z a -tzip -p"malte" -r "Win-Atlas Playbook.apbx" "./src/playbook"
+7z a -tzip -p"malte" -r "Atlas Playbook.apbx" "./src/playbook"
 
 # Copy to, and zip release
-Copy-Item -Path "Win-Atlas Playbook.apbx" -Destination "./src/release-zip" -Force
+Copy-Item -Path "Atlas Playbook.apbx" -Destination "./src/release-zip" -Force
 Compress-Archive -Path "./src/release-zip/*" -DestinationPath "./win-atlas.zip"
